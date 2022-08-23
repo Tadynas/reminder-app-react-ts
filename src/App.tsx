30aee66496673ba45950258d4f5c9c00ed0e73ac
Tadynas <griciustadas@gmail.com>
2022-08-23 14:38:41 +0300

    Creating component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/reminder';
+
+const reminders: Reminder[] = [
+  { id: 1, title: 'Reminder 1'}
+]
 
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-primary">Click Me</button>
+      <ReminderList items={reminders} />
     </div>
   );
 }
