47500f510b27ecce9bf8eecf40af32c78769cd05
Tadynas <griciustadas@gmail.com>
2022-08-23 14:55:56 +0300

    Handling events

--- src/components/ReminderList.tsx
+++ src/components/ReminderList.tsx
@@ -3,12 +3,18 @@ import Reminder from '../models/reminder'
 
 interface ReminderListProps {
     items: Reminder[]
+    onRemoveReminder: (id: number) => void
 }
 
-function ReminderList({ items }: ReminderListProps) {
+function ReminderList({ items, onRemoveReminder }: ReminderListProps) {
     return (
         <ul className="list-group">
-            {items.map(item => <li className="list-group-item" key={item.id}>{item.title}</li>)}
+            {items.map(item => 
+                <li className="list-group-item" key={item.id}>
+                    {item.title}
+                    <button onClick={() => onRemoveReminder(item.id)} className="btn btn-outline-danger mx-2">Delete</button>
+                </li>
+            )}
         </ul>
     )
 }
